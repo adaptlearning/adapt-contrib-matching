041586db53b8d258a2b7cc8b44bd8ab0fc551592
Kevin Corry <kevinc@learningpool.com>
2013-12-20 15:48:03 +0000

    Work in progress

---
+++ js/adapt-contrib-matching.js
@@ -0,0 +1,96 @@
+/*
+* adapt-contrib-matching
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Kevin Corry <kevinc@learningpool.com>
+*/
+define(function(require) {
+  var Adapt = require('coreJS/adapt');
+  var QuestionView = require('coreViews/questionView');
+
+  var Matching = QuestionView.extend({
+    
+    events: {
+      "change .matching-select": "onSelectChanged",
+      "click .button.submit":"onSubmitClicked",
+      "click .button.reset":"onResetClicked",
+      "click .button.model":"onModelAnswerClicked",
+      "click .button.user":"onUserAnswerClicked"
+    },
+
+    postRender: function() {
+      this.setReadyStatus();
+    },
+    
+    canSubmit: function() {
+      var canSubmit = true;
+      $('.matching-select option:selected',this.el).each(
+        _.bind(function(index, element) {
+          var $element = $(element);
+          if ($element.index()==0) {
+            canSubmit = false;
+            $element.parent('.matching-select').addClass('error');
+          }
+        }, this));
+      return canSubmit;
+    },
+
+    forEachAnswer: function(callback) {
+      _.each(this.model.get('items'), function(item, index) {
+        var $selectedOption = this.$('.matching-select option:selected').eq(index);
+        var correctSelection = item.options[$selectedOption.index()-1].correct;
+        if(correctSelection) this.model.set('_isAtLeastOneCorrectSelection', true);
+        callback(correctSelection, item);
+      }, this);
+    },
+
+    markQuestion: function() {
+      this.forEachAnswer(function(correct, item) {
+        item.correct = correct;
+      });
+      QuestionView.prototype.markQuestion.apply(this);
+    },
+
+    onEnabledChanged: function() {
+      this.$('.matching-select').prop('disabled', !this.model.get('_isEnabled'));
+    },
+
+    onModelAnswerShown: function() {
+      _.each(this.model.get('items'), function(item, index) {
+        var correctOptionIndex;
+        _.each(item.options, function(option, optionIndex) {
+          if(option.correct) correctOptionIndex = optionIndex + 1;
+        }, this);
+        var $parent = this.$('.matching-select').eq(index);
+        this.selectOption($parent, correctOptionIndex);
+      }, this);
+    },
+
+    onSelectChanged: function(event) {
+      $(event.target).blur();
+    },
+
+    onUserAnswerShown: function(event) {
+      for(var i = 0, count = this.model.get('items').length; i < count; i++) {
+        var $parent = this.$('.matching-select').eq(i);
+        this.selectOption($parent, this.model.get('_userAnswer')[i]);
+      }
+    },
+
+    selectOption: function($parent, optionIndex) {
+      $("option", $parent).eq(optionIndex).attr('selected', 'selected');
+      $parent[0].selectedIndex = optionIndex; 
+    },
+
+    storeUserAnswer: function() {
+      var userAnswer = [];
+      _.each(this.model.get('items'), function(item, index) {
+        var $selectedOption = this.$('.matching-select option:selected').eq(index);
+        userAnswer.push($selectedOption.index());
+      }, this);
+      this.model.set('_userAnswer', userAnswer);
+    }
+  });
+
+  Adapt.register("matching", Matching);
+
+});
