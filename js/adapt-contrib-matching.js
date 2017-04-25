edc4ccf7fa2c213174e58b0335cfc79b8b53b397
Oliver Foster <oliver.foster@kineo.com>
2017-04-25 16:55:05 +0100

    issue/1543: added source comment

--- js/adapt-contrib-matching.js
+++ js/adapt-contrib-matching.js
@@ -4,6 +4,9 @@ define([
     'libraries/select2'
 ],function(Adapt, QuestionView) {
     
+    /*
+     * issue/1543: fix from https://github.com/select2/select2/issues/4063
+     */
     var dropdownAdapter;
     jQuery.fn.select2.amd.require([    
         "select2/utils",
