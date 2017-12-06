6d4ce09f470ee0ba61095d1423519621a8dab9d8
Oliver Foster <oliver.foster@kineo.com>
2017-12-06 18:39:00 +0000

    issue/1889: drop down appears above page bottom

--- js/adapt-contrib-matching.js
+++ js/adapt-contrib-matching.js
@@ -15,6 +15,71 @@ define([
         "select2/dropdown/closeOnSelect"
     ], function(Utils, DropdownAdapter, AttachContainer, CloseOnSelect) {
 
+        // code added to make dropdown sit above body bottom where appropriate
+        AttachContainer.prototype.bind = function(decorated, container, $container) {
+
+            var self = this;
+            decorated.call(this, $container, $container);
+
+            container.on('open', function () {
+                setTimeout(function() {
+                    self.position(self.$dropdown, $container);
+                }, 0);
+            });
+
+        };
+
+        AttachContainer.prototype.position = function(decorated, $dropdown, $container) {
+
+            var $dropdownContainer = $container.find('.dropdown-wrapper');
+            $dropdownContainer.append($dropdown);
+
+            var $window = $(window);
+
+            var viewport = {
+              top: $window.scrollTop(),
+              bottom: $window.scrollTop() + $window.height()
+            };
+
+            var container = $container.offset();
+            container.height = $container.outerHeight(false);
+            container.bottom = container.top + container.height;
+
+            var dropdown = {
+              height: $dropdown.outerHeight(false)
+            };
+
+            var viewport = {
+              top: $window.scrollTop(),
+              bottom: $window.scrollTop() + $window.height()
+            };
+
+            var enoughRoomBelow = !dropdown.height || viewport.bottom > (container.bottom + dropdown.height);
+
+            var oldDirection = $dropdown.hasClass('select2-dropdown--above') ? "above" : $dropdown.hasClass('select2-dropdown--below') ? "below" : "none";
+            var newDirection = !enoughRoomBelow ? "above" : "below";
+
+            if (newDirection === oldDirection) return;
+
+            $dropdown
+                .removeClass('select2-dropdown--below select2-dropdown--above')
+                .addClass('select2-dropdown--' + newDirection);
+            $container
+                .removeClass('select2-container--below select2-container--above')
+                .addClass('select2-container--' + newDirection);
+
+            switch (newDirection) {
+                case "below":
+                    $dropdown.css("bottom", "");
+                    break;
+                case "above":
+                    $dropdown.css("bottom", container.height);
+                    break;
+            }
+
+        };
+
+        // override default AttachBody
         dropdownAdapter = Utils.Decorate(
             Utils.Decorate(
                 DropdownAdapter,
