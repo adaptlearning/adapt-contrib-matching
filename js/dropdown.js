40bb50d16f4116ee2ce74335f770b233b4fcf750
Oliver foster <oliver.foster@kineo.com>
2019-01-21 15:45:57 +0000

    Converted to 4space

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,293 +1,293 @@
 define([
-  './dropdownOption',
-  'handlebars'
+    './dropdownOption',
+    'handlebars'
 ], function(DropDownOption, Handlebars) {
 
-  var DropDown = Backbone.View.extend({
+    var DropDown = Backbone.View.extend({
 
-    initialize: function(settings) {
-      _.bindAll(this, "onStartInteraction", "onNativeSelectChange", "onButtonClick", "onListBlur", "onKeyDown");
-      this.settings = _.defaults(settings, this.getDefaults());
-      this.placeholder = null;
-      this.options = [];
-      this.setUpElements();
-      this.setUpItems();
-      this.select(this.settings.value);
-      this.addEventListeners();
-      this.toggleOpen(false);
-      this.settings.load.call(this, this);
-      this.trigger("load", this);
-    },
+        initialize: function(settings) {
+            _.bindAll(this, "onStartInteraction", "onNativeSelectChange", "onButtonClick", "onListBlur", "onKeyDown");
+            this.settings = _.defaults(settings, this.getDefaults());
+            this.placeholder = null;
+            this.options = [];
+            this.setUpElements();
+            this.setUpItems();
+            this.select(this.settings.value);
+            this.addEventListeners();
+            this.toggleOpen(false);
+            this.settings.load.call(this, this);
+            this.trigger("load", this);
+        },
 
-    getDefaults: function() {
-      return {
-        useNative: false,
-        load: DropDown.defaults.load,
-        openList: DropDown.defaults.openList,
-        closeList: DropDown.defaults.closeList,
-        scrollToItem: DropDown.defaults.scrollToItem
-      };
-    },
+        getDefaults: function() {
+            return {
+                useNative: false,
+                load: DropDown.defaults.load,
+                openList: DropDown.defaults.openList,
+                closeList: DropDown.defaults.closeList,
+                scrollToItem: DropDown.defaults.scrollToItem
+            };
+        },
 
-    setUpElements: function() {
-      if (this.$el.is(".is-native")) {
-        this.settings.useNative = true;
-        this.$button = this.$el;
-        this.$input = this.$list = this.$("select");
-        return;
-      }
-      this.$list = this.$(this.getTagName());
-      this.$button = this.$("button");
-      this.$inner = this.$("button .dropdown__inner");
-      this.$input = this.$("input");
-    },
+        setUpElements: function() {
+            if (this.$el.is(".is-native")) {
+                this.settings.useNative = true;
+                this.$button = this.$el;
+                this.$input = this.$list = this.$("select");
+                return;
+            }
+            this.$list = this.$(this.getTagName());
+            this.$button = this.$("button");
+            this.$inner = this.$("button .dropdown__inner");
+            this.$input = this.$("input");
+        },
 
-    getTagName: function() {
-      if (this.isNativeSelect()) return 'select';
-      return 'ul';
-    },
+        getTagName: function() {
+            if (this.isNativeSelect()) return 'select';
+            return 'ul';
+        },
 
-    setUpItems: function() {
-      var $options = this.$(this.getItemTagName());
-      $options.each(function(index, el) {
-        new DropDownOption({
-          parent: this,
-          el: el
-        });
-      }.bind(this));
-    },
+        setUpItems: function() {
+            var $options = this.$(this.getItemTagName());
+            $options.each(function(index, el) {
+                new DropDownOption({
+                    parent: this,
+                    el: el
+                });
+            }.bind(this));
+        },
 
-    getItemTagName: function() {
-      if (this.isNativeSelect()) return 'option';
-      return 'li';
-    },
+        getItemTagName: function() {
+            if (this.isNativeSelect()) return 'option';
+            return 'li';
+        },
 
-    isNativeSelect: function() {
-      return this.settings.useNative;
-    },
+        isNativeSelect: function() {
+            return this.settings.useNative;
+        },
 
-    addEventListeners: function() {
-      if (this.isNativeSelect()) {
-        this.$list.on({
-          'mousedown touchstart': this.onStartInteraction,
-          change: this.onNativeSelectChange
-        });
-        return;
-      }
+        addEventListeners: function() {
+            if (this.isNativeSelect()) {
+                this.$list.on({
+                    'mousedown touchstart': this.onStartInteraction,
+                    change: this.onNativeSelectChange
+                });
+                return;
+            }
 
-      this.$button.on({
-        'mousedown touchstart': this.onStartInteraction,
-        click: this.onButtonClick
-      });
-      this.$list.on('blur', this.onListBlur);
-      $(document).on('keydown', this.onKeyDown);
-    },
+            this.$button.on({
+                'mousedown touchstart': this.onStartInteraction,
+                click: this.onButtonClick
+            });
+            this.$list.on('blur', this.onListBlur);
+            $(document).on('keydown', this.onKeyDown);
+        },
 
-    onStartInteraction: function() {
-      this.wasOpen = this.isOpen();
-    },
+        onStartInteraction: function() {
+            this.wasOpen = this.isOpen();
+        },
 
-    onNativeSelectChange: function() {
-      this.deselectAll();
-      var controlValue = String(this.$list.val());
-      _.find(this.options, function(option) {
-        if (controlValue !== option.getValue()) return;
-        option.select();
-        return true;
-      });
-    },
+        onNativeSelectChange: function() {
+            this.deselectAll();
+            var controlValue = String(this.$list.val());
+            _.find(this.options, function(option) {
+                if (controlValue !== option.getValue()) return;
+                option.select();
+                return true;
+            });
+        },
 
-    deselectAll: function() {
-      this.placeholder.deselect();
-      this.options.forEach(function(option) {
-        option.deselect();
-      });
-    },
+        deselectAll: function() {
+            this.placeholder.deselect();
+            this.options.forEach(function(option) {
+                option.deselect();
+            });
+        },
 
-    onButtonClick: function(event) {
-      if (this.wasOpen || this.isOpen()) {
-        // click toggle list:
-        // if the list is open and the button is clicked
-        // the blur event will fire first closing the list.
-        // this code is to prevent the list from reopening
-        // in that situation
-        return this.wasOpen = false;
-      }
-      this.toggleOpen();
-      var option = this.getFirstSelectedItem() || this.placeholder;
-      if (!option) return;
-      option.reselect();
-      option.scrollTo();
-      this.$list.focus();
-    },
+        onButtonClick: function(event) {
+            if (this.wasOpen || this.isOpen()) {
+                // click toggle list:
+                // if the list is open and the button is clicked
+                // the blur event will fire first closing the list.
+                // this code is to prevent the list from reopening
+                // in that situation
+                return this.wasOpen = false;
+            }
+            this.toggleOpen();
+            var option = this.getFirstSelectedItem() || this.placeholder;
+            if (!option) return;
+            option.reselect();
+            option.scrollTo();
+            this.$list.focus();
+        },
 
-    isOpen: function() {
-      return !this.$list.hasClass('hidden');
-    },
+        isOpen: function() {
+            return !this.$list.hasClass('hidden');
+        },
 
-    toggleOpen: function(open) {
-      if (this.isNativeSelect()) return;
-      if (open === undefined) open = !this.isOpen();
-      this.$button.attr('aria-expanded', open ? 'true' : 'false');
-      var name = open ? 'openList' : 'closeList';
-      this.settings[name].call(this, this);
-      this.trigger(name, this);
-    },
+        toggleOpen: function(open) {
+            if (this.isNativeSelect()) return;
+            if (open === undefined) open = !this.isOpen();
+            this.$button.attr('aria-expanded', open ? 'true' : 'false');
+            var name = open ? 'openList' : 'closeList';
+            this.settings[name].call(this, this);
+            this.trigger(name, this);
+        },
 
-    onListBlur: function() {
-      this.toggleOpen(false);
-      this.removeActiveDescendentId();
-    },
+        onListBlur: function() {
+            this.toggleOpen(false);
+            this.removeActiveDescendentId();
+        },
 
-    onKeyDown: function(event) {
-      if (!this.isOpen()) return;
-      var option = this.getFirstSelectedItem() || this.placeholder;
-      switch (event.keyCode) {
-        case 38: // UP
-          event.preventDefault();
-          option = option.getPrevious() || option.getLast();
-          break;
-        case 40: // DOWN
-          event.preventDefault();
-          option = option.getNext() || option.getFirst();
-          break;
-        case 13: // ENTER
-        case 32: // SPACE
-          event.preventDefault();
-          this.$button.focus();
-          return;
-        case 27: // ESCAPE
-          event.preventDefault();
-          this.$button.focus();
-          return;
-        default:
-          return;
-      }
-      this.deselectAll();
-      option.select().scrollTo();
-    },
+        onKeyDown: function(event) {
+            if (!this.isOpen()) return;
+            var option = this.getFirstSelectedItem() || this.placeholder;
+            switch (event.keyCode) {
+                case 38: // UP
+                    event.preventDefault();
+                    option = option.getPrevious() || option.getLast();
+                    break;
+                case 40: // DOWN
+                    event.preventDefault();
+                    option = option.getNext() || option.getFirst();
+                    break;
+                case 13: // ENTER
+                case 32: // SPACE
+                    event.preventDefault();
+                    this.$button.focus();
+                    return;
+                case 27: // ESCAPE
+                    event.preventDefault();
+                    this.$button.focus();
+                    return;
+                default:
+                    return;
+            }
+            this.deselectAll();
+            option.select().scrollTo();
+        },
 
-    getFirstSelectedItem: function() {
-      return _.find(this.options, function(option) {
-        if (option.isSelected()) return option;
-      });
-    },
+        getFirstSelectedItem: function() {
+            return _.find(this.options, function(option) {
+                if (option.isSelected()) return option;
+            });
+        },
 
-    setActiveDescendentId: function(id) {
-      if (this.isNativeSelect()) return;
-      this.$list.attr('aria-activedescendant', id);
-    },
+        setActiveDescendentId: function(id) {
+            if (this.isNativeSelect()) return;
+            this.$list.attr('aria-activedescendant', id);
+        },
 
-    removeActiveDescendentId: function() {
-      this.$list.removeAttr('aria-activedescendant');
-    },
+        removeActiveDescendentId: function() {
+            this.$list.removeAttr('aria-activedescendant');
+        },
 
-    select: function(value) {
-      value = String(value);
-      var option = _.find(this.options, function(option) {
-        if (option.getValue() !== value) return false
-        option.select();
-        return true
-      });
-      if (option) return;
-      this.placeholder.select();
-    },
+        select: function(value) {
+            value = String(value);
+            var option = _.find(this.options, function(option) {
+                if (option.getValue() !== value) return false
+                option.select();
+                return true
+            });
+            if (option) return;
+            this.placeholder.select();
+        },
 
-    toggleDisabled: function(value) {
-      if (value === undefined) value != this.$input.attr("disabled");
-      if (value === false) {
-        this.$input.removeAttr("disabled");
-        this.$button.removeAttr("disabled");
-        this.$el.removeAttr("disabled");
-        return;
-      }
-      this.$input.attr("disabled", "");
-      this.$button.attr("disabled", "");
-      this.$el.attr("disabled", "");
-    },
+        toggleDisabled: function(value) {
+            if (value === undefined) value != this.$input.attr("disabled");
+            if (value === false) {
+                this.$input.removeAttr("disabled");
+                this.$button.removeAttr("disabled");
+                this.$el.removeAttr("disabled");
+                return;
+            }
+            this.$input.attr("disabled", "");
+            this.$button.attr("disabled", "");
+            this.$el.attr("disabled", "");
+        },
 
-    isEmpty: function() {
-      return !this.getFirstSelectedItem();
-    },
+        isEmpty: function() {
+            return !this.getFirstSelectedItem();
+        },
 
-    val: function() {
-      return this.$input.val();
-    },
+        val: function() {
+            return this.$input.val();
+        },
 
-    destroy: function() {
-      this.removeEventListeners();
-      this.$el.remove();
-      delete this.options;
-      delete this.$input;
-      delete this.$inner;
-      delete this.$button;
-      delete this.$list;
-      delete this.$el;
-    },
+        destroy: function() {
+            this.removeEventListeners();
+            this.$el.remove();
+            delete this.options;
+            delete this.$input;
+            delete this.$inner;
+            delete this.$button;
+            delete this.$list;
+            delete this.$el;
+        },
 
-    removeEventListeners: function() {
-      if (this.isNativeSelect()) {
-        this.$list.off({
-          'mousedown touchstart': this.onStartInteraction,
-          change: this.onNativeSelectChange
-        });
-        return;
-      }
-      this.$button.off({
-        'mousedown touchstart': this.onStartInteraction,
-        click: this.onButtonClick
-      });
-      this.$list.off('blur', this.onListBlur);
-      $(document).off({
-        keydown: this.onKeyDown
-      });
-    }
+        removeEventListeners: function() {
+            if (this.isNativeSelect()) {
+                this.$list.off({
+                    'mousedown touchstart': this.onStartInteraction,
+                    change: this.onNativeSelectChange
+                });
+                return;
+            }
+            this.$button.off({
+                'mousedown touchstart': this.onStartInteraction,
+                click: this.onButtonClick
+            });
+            this.$list.off('blur', this.onListBlur);
+            $(document).off({
+                keydown: this.onKeyDown
+            });
+        }
 
-  }, {
+    }, {
 
-    defaults: {
+        defaults: {
 
-      load: function() {},
+            load: function() {},
 
-      openList: function() {
-        this.$list
-          .css({
-            left: this.$button[0].offsetLeft,
-            width: this.$button[0].offsetWidth
-          })
-          .addClass('sizing')
-          .removeClass('hidden');
+            openList: function() {
+                this.$list
+                    .css({
+                        left: this.$button[0].offsetLeft,
+                        width: this.$button[0].offsetWidth
+                    })
+                    .addClass('sizing')
+                    .removeClass('hidden');
 
-        var offset = this.$list[0].getBoundingClientRect();
-        var height = this.$list.height();
-        var windowHeight = $(window).height();
-        var isOffscreen = (offset.top + height > windowHeight);
+                var offset = this.$list[0].getBoundingClientRect();
+                var height = this.$list.height();
+                var windowHeight = $(window).height();
+                var isOffscreen = (offset.top + height > windowHeight);
 
-        this.$list
-          .css({
-            top: isOffscreen ? -height : ''
-          })
-          .removeClass('sizing')
-          .focus();
+                this.$list
+                    .css({
+                        top: isOffscreen ? -height : ''
+                    })
+                    .removeClass('sizing')
+                    .focus();
 
-      },
+            },
 
-      closeList: function() {
-        this.$list
-          .removeClass('sizing')
-          .addClass('hidden', true);
-      },
+            closeList: function() {
+                this.$list
+                    .removeClass('sizing')
+                    .addClass('hidden', true);
+            },
 
-      scrollToItem: function(option) {
-        var height = this.$list[0].clientHeight;
-        var pos = option.$el[0].offsetTop-(height/2);
-        this.$list[0].scrollTop = pos;
-      }
+            scrollToItem: function(option) {
+                var height = this.$list[0].clientHeight;
+                var pos = option.$el[0].offsetTop-(height/2);
+                this.$list[0].scrollTop = pos;
+            }
 
-    }
+        }
 
-  });
+    });
 
-  return DropDown;
+    return DropDown;
 
 });
--- js/dropdownOption.js
+++ js/dropdownOption.js
@@ -1,147 +1,147 @@
 define(function() {
 
-  var DropDownItem = Backbone.View.extend({
+    var DropDownItem = Backbone.View.extend({
 
-    initialize: function(settings) {
-      _.bindAll(this, 'onClick');
-      this.settings = settings;
-      this.$inner = this.$el.find('span');
-      this.addToParent();
-      this.setUpEventListeners();
-    },
+        initialize: function(settings) {
+            _.bindAll(this, 'onClick');
+            this.settings = settings;
+            this.$inner = this.$el.find('span');
+            this.addToParent();
+            this.setUpEventListeners();
+        },
 
-    addToParent: function() {
-      var parent = this.parent();
-      if (this.isPlaceholder()) {
-        parent.placeholder = this;
-      } else {
-        parent.options.push(this);
-      }
-      parent.$list.append(this.$el);
-    },
+        addToParent: function() {
+            var parent = this.parent();
+            if (this.isPlaceholder()) {
+                parent.placeholder = this;
+            } else {
+                parent.options.push(this);
+            }
+            parent.$list.append(this.$el);
+        },
 
-    isPlaceholder: function() {
-      return this.$el.is('[hidden]');
-    },
+        isPlaceholder: function() {
+            return this.$el.is('[hidden]');
+        },
 
-    parent: function() {
-      return this.settings.parent;
-    },
+        parent: function() {
+            return this.settings.parent;
+        },
 
-    isNativeSelect: function() {
-      return this.parent().isNativeSelect();
-    },
+        isNativeSelect: function() {
+            return this.parent().isNativeSelect();
+        },
 
-    setUpEventListeners: function() {
-      if (this.isNativeSelect()) return;
-      this.$el.on("click", this.onClick);
-    },
+        setUpEventListeners: function() {
+            if (this.isNativeSelect()) return;
+            this.$el.on("click", this.onClick);
+        },
 
-    onClick: function(event) {
-      var parent = this.parent();
-      event.preventDefault();
-      parent.deselectAll();
-      this.select().scrollTo();
-      parent.$button.focus();
-    },
+        onClick: function(event) {
+            var parent = this.parent();
+            event.preventDefault();
+            parent.deselectAll();
+            this.select().scrollTo();
+            parent.$button.focus();
+        },
 
-    getValue: function() {
-      return this.$el.attr('value').trim();
-    },
+        getValue: function() {
+            return this.$el.attr('value').trim();
+        },
 
-    select: function() {
-      var parent = this.parent();
-      parent.setActiveDescendentId(this.$el[0].id);
-      this.$el.attr('selected', '');
-      parent.$input.val(this.getValue());
-      if (this.isNativeSelect()) {
-        parent.trigger('change', parent);
-        return this;
-      }
-      this.$el.attr('aria-selected', 'true');
-      parent.$inner.html(this.$el.attr('text'));
-      parent.$input
-        .attr('value', this.$el.attr('value'))
-        .trigger('change');
-      parent.trigger('change', parent);
-      return this;
-    },
+        select: function() {
+            var parent = this.parent();
+            parent.setActiveDescendentId(this.$el[0].id);
+            this.$el.attr('selected', '');
+            parent.$input.val(this.getValue());
+            if (this.isNativeSelect()) {
+                parent.trigger('change', parent);
+                return this;
+            }
+            this.$el.attr('aria-selected', 'true');
+            parent.$inner.html(this.$el.attr('text'));
+            parent.$input
+                .attr('value', this.$el.attr('value'))
+                .trigger('change');
+            parent.trigger('change', parent);
+            return this;
+        },
 
-    deselect: function() {
-      var parent = this.parent();
-      this.$el.removeAttr('selected');
-      if (this.isNativeSelect()) return this;
-      this.$el.attr('aria-selected', 'false');
-      return this;
-    },
+        deselect: function() {
+            var parent = this.parent();
+            this.$el.removeAttr('selected');
+            if (this.isNativeSelect()) return this;
+            this.$el.attr('aria-selected', 'false');
+            return this;
+        },
 
-    reselect: function() {
-      var parent = this.parent();
-      parent.setActiveDescendentId(this.$el[0].id);
-      if (this.isSelected()) return this;
-      parent.deselectAll();
-      this.select();
-      return this;
-    },
+        reselect: function() {
+            var parent = this.parent();
+            parent.setActiveDescendentId(this.$el[0].id);
+            if (this.isSelected()) return this;
+            parent.deselectAll();
+            this.select();
+            return this;
+        },
 
-    isSelected: function() {
-      return Boolean(this.$el.attr('selected'));
-    },
+        isSelected: function() {
+            return Boolean(this.$el.attr('selected'));
+        },
 
-    getNext: function() {
-      var parent = this.parent();
-      var found = null;
-      _.find(parent.options, function(option, index) {
-        if (option !== this) return;
-        return found = parent.options[index+1];
-      }.bind(this));
-      return found;
-    },
+        getNext: function() {
+            var parent = this.parent();
+            var found = null;
+            _.find(parent.options, function(option, index) {
+                if (option !== this) return;
+                return found = parent.options[index+1];
+            }.bind(this));
+            return found;
+        },
 
-    getPrevious: function() {
-      var parent = this.parent();
-      var found = null;
-       _.find(parent.options, function(option, index) {
-        if (option !== this) return;
-        return found = parent.options[index-1];
-      }.bind(this));
-      return found;
-    },
+        getPrevious: function() {
+            var parent = this.parent();
+            var found = null;
+             _.find(parent.options, function(option, index) {
+                if (option !== this) return;
+                return found = parent.options[index-1];
+            }.bind(this));
+            return found;
+        },
 
-    getFirst: function() {
-      var parent = this.parent();
-      return parent.options[0];
-    },
+        getFirst: function() {
+            var parent = this.parent();
+            return parent.options[0];
+        },
 
-    getLast: function() {
-      var parent = this.parent();
-      return parent.options[parent.options.length-1];
-    },
+        getLast: function() {
+            var parent = this.parent();
+            return parent.options[parent.options.length-1];
+        },
 
-    scrollTo: function() {
-      var parent = this.parent();
-      parent.settings.scrollToItem.call(parent, this);
-    },
+        scrollTo: function() {
+            var parent = this.parent();
+            parent.settings.scrollToItem.call(parent, this);
+        },
 
-    destroy: function() {
-      var parent = this.parent();
-      this.$el.remove();
-      if (this.isPlaceholder()) {
-        parent.placeholder = null;
-      } else {
-        for (var i = 0, l = parent.options.length; i < l; i++) {
-          var item = parent.options[i];
-          if (item !== this) continue;
-          parent.options.splice(i, 1);
-          break;
+        destroy: function() {
+            var parent = this.parent();
+            this.$el.remove();
+            if (this.isPlaceholder()) {
+                parent.placeholder = null;
+            } else {
+                for (var i = 0, l = parent.options.length; i < l; i++) {
+                    var item = parent.options[i];
+                    if (item !== this) continue;
+                    parent.options.splice(i, 1);
+                    break;
+                }
+            }
+            delete this.$el;
+            delete this.settings;
         }
-      }
-      delete this.$el;
-      delete this.settings;
-    }
 
-  });
+    });
 
-  return DropDownItem;
+    return DropDownItem;
 
 });
