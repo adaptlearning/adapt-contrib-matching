e68559762ea2b5bcef1c2abf86bd4517df407f0d
Oliver Foster <oliver.foster@kineo.com>
2019-07-16 11:43:58 +0100

    issue/2459 force disabled old ios fixes in framework once installed (#118)

--- js/adapt-contrib-matching.js
+++ js/adapt-contrib-matching.js
@@ -4,6 +4,12 @@ define([
     './matchingModel'
 ], function(Adapt, MatchingView, MatchingModel) {
 
+    // Force disable old iOS fixes in v2 frameworks
+    // https://github.com/adaptlearning/adapt_framework/issues/2459
+    if ($.a11y && $.a11y.options && $.a11y.options.isIOSFixesEnabled) {
+        $.a11y.options.isIOSFixesEnabled = false;
+    }
+
     return Adapt.register("matching", {
         view: MatchingView,
         model: MatchingModel
