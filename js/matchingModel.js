31715ab1fe495310226147fca1d21bf35ce1eab3
Oliver Foster <oliver.foster@kineo.com>
2021-06-22 09:21:25 +0000

    issue/2464 Automatically focus on first item (#131)

--- js/matchingModel.js
+++ js/matchingModel.js
@@ -67,6 +67,11 @@ define([
 
     setOptionSelected: function(itemIndex, optionIndex, isSelected) {
       var item = this.get('_items')[itemIndex];
+      if (isNaN(optionIndex)) {
+        item._options.forEach(option => option._isSelected = false);
+        item._selected = null;
+        return this.checkCanSubmit();
+      }
       var option = _.findWhere(item._options, { '_index': optionIndex });
       option._isSelected = isSelected;
       item._selected = option;
