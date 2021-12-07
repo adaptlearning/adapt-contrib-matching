1fb8d5b8032c047fe343d73baf3c813ea8b2b59a
Oliver Foster <oliver.foster@kineo.com>
2021-12-07 09:57:59 +0000

    issue/2515 Removed comments

--- js/MatchingView.js
+++ js/MatchingView.js
@@ -17,25 +17,6 @@ class MatchingView extends QuestionView {
     this.setReadyStatus();
   }
 
-  // showCorrectAnswer() {
-  //   this.model.get('_items').forEach(({ _options }, index) => {
-  //     const correctOption = _options.find(({ _isCorrect }) => _isCorrect);
-  //     this.selectValue(index, correctOption._index);
-  //   });
-  // }
-
-  // hideCorrectAnswer() {
-  //   const answerArray = this.model.has('_tempUserAnswer') ?
-  //     this.model.get('_tempUserAnswer') :
-  //     this.model.get('_userAnswer');
-
-  //   this.model.get('_items').forEach(({ _options }, index) => {
-  //     const key = answerArray[index];
-  //     const value = _options[key]._index;
-  //     this.selectValue(index, value);
-  //   });
-  // }
-
 }
 
 MatchingView.template = 'matching.jsx';
