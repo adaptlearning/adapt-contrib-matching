cdcd7725ee030ad1ad6f4d9a2ca5f77ed666ab39
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2023-02-18 01:51:12 +0000

    Fix: u-no-select class added to dropdown item text

--- templates/matchingDropDown.jsx
+++ templates/matchingDropDown.jsx
@@ -225,7 +225,7 @@ export default function MatchingDropDown(props) {
             selected={_isHighlighted || null}
             onClick={onOptionClicked}
           >
-            <div className="dropdown-item__inner js-dropdown-list-item-inner" dangerouslySetInnerHTML={{ __html: displayText || text }}>
+            <div className="dropdown-item__inner js-dropdown-list-item-inner u-no-select" dangerouslySetInnerHTML={{ __html: displayText || text }}>
             </div>
           </li>;
         })}
