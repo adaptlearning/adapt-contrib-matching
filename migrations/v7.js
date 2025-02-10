4f148a615d41c7cfb7e2a6a9d497215ffb7e84e0
Brad Simpson <brad.simpson@kineo.com>
2025-02-10 12:19:04 -0700

    7.2.1 scripts

--- migrations/v7.js
+++ migrations/v7.js
@@ -26,3 +26,21 @@ describe('Matching - v6.0.0 to v7.2.0', async () => {
   });
   updatePlugin('Matching - update to v7.2.0', { name: 'adapt-contrib-matching', version: '7.2.0', framework: '>=5.19.1' });
 });
+
+describe('Matching - v7.2.0 to v7.2.1', async () => {
+  whereFromPlugin('Matching - from v7.2.1', { name: 'adapt-contrib-matching', version: '<7.2.1' });
+  whereContent('Matching - where matching', async content => {
+    matchings = content.filter(({ _component }) => _component === 'matching');
+    return matchings.length;
+  });
+  mutateContent('Matching - add ariaQuestion attribute', async content => {
+    matchings.forEach(matching => (matching.ariaQuestion = ''));
+    return true;
+  });
+  checkContent('Matching - check ariaQuestion attribute', async content => {
+    const isValid = matchings.every(matching => matching.ariaQuestion === '');
+    if (!isValid) throw new Error('Matching - ariaQuestion attribute invalid');
+    return true;
+  });
+  updatePlugin('Matching - update to v7.2.1', { name: 'adapt-contrib-matching', version: '7.2.1', framework: '>=5.19.1' });
+});
