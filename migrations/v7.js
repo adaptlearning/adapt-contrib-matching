ca45595bbe64ce7aaf0254cd1ed7291f1b1a75d1
Brad Simpson <brad.simpson@kineo.com>
2025-02-10 11:39:35 -0700

    7.2.0 scripts

--- migrations/v7.js
+++ migrations/v7.js
@@ -1,5 +1,5 @@
 import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
-
+// import _ from 'lodash';
 let matchings;
 
 describe('Matching - v6.0.0 to v7.2.0', async () => {
@@ -8,23 +8,21 @@ describe('Matching - v6.0.0 to v7.2.0', async () => {
     matchings = content.filter(({ _component }) => _component === 'matching');
     return matchings.length;
   });
-  // mutateContent('Matching - add _hasItemScoring attribute', async content => {
-  //   matchings.forEach(matching => { matching._hasItemScoring = false; });
-  //   return true;
-  // });
-  // mutateContent('Matching - add _allowOnlyUniqueAnswers attribute', async content => {
-  //   matchings.forEach(matching => { matching._allowOnlyUniqueAnswers = false; });
-  //   return true;
-  // });
-  // checkContent('Matching - check _hasItemScoring attribute', async content => {
-  //   const isValid = matchings.every(({ _hasItemScoring }) => _hasItemScoring === false);
-  //   if (!isValid) throw new Error('Matching - _hasItemScoring attribute invalid');
-  //   return true;
-  // });
-  // checkContent('Matching - check _allowOnlyUniqueAnswers attribute', async content => {
-  //   const isValid = matchings.every(({ _allowOnlyUniqueAnswers }) => _allowOnlyUniqueAnswers === false);
-  //   if (!isValid) throw new Error('Matching - _allowOnlyUniqueAnswers attribute invalid');
-  //   return true;
-  // });
+  mutateContent('Matching - update instruction attribute', async content => {
+    const originalInstruction = '';
+    matchings.forEach(matching => {
+      if (matching.instruction === originalInstruction) {
+        matching.instruction = 'Choose an option from each dropdown list and select Submit.';
+      }
+    });
+    return true;
+  });
+  checkContent('Matching - check instruction attribute', async content => {
+    const isValid = matchings.every(({ instruction }) => {
+      return instruction === 'Choose an option from each dropdown list and select Submit.';
+    });
+    if (!isValid) throw new Error('Matching - instruction attribute invalid');
+    return true;
+  });
   updatePlugin('Matching - update to v7.2.0', { name: 'adapt-contrib-matching', version: '7.2.0', framework: '>=5.19.1' });
 });
